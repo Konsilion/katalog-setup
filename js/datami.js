b14abe9fa63c4171c60c5ed098c1f358287b1118
Nicolas BREMOND <99027754+Konsilion@users.noreply.github.com>
2022-11-27 12:46:29 +0100

    Create datami.js

---
+++ js/datami.js
@@ -0,0 +1,79 @@
+function DatamiConstruction(name,link) {
+
+    let grid = document.getElementById('DatamiGrid');
+    
+    let html = `<!-- DATAMI WIDGET'S HTML BLOCK -->
+                <datami-file
+                  title="` + name + `"
+                  gitfile="` + link + `"
+                  options='{
+                  "height": "500px",
+                  "separator": ";",
+                  "lockcolumns": false,
+                  "pagination": {
+                    "itemsPerPage": 25
+                  },
+                  "cardsview": {
+                    "activate": true,
+                    "default": true
+                  },
+                  "schema": {
+                    "file": "https://github.com/Konsilion/katalog-setup/blob/master/json/schema.json"
+                  },
+                  "fields-custom-properties": {
+                    "file": "https://github.com/Konsilion/katalog-setup/blob/master/json/custom.json"
+                  },
+                  "customfilters": {
+                    "activate": true,
+                    "filterfields": [
+                      "IDs - FILTRES"
+                    ],
+                    "tagsSeparator": ","
+                  },
+
+                  "cardsdetail": false,
+                  "cardssettings": {
+                    "mini": {
+                      "NOM DE LA RESSOURCE": {
+                        "position": "title"
+                      },
+                      "DESCRIPTION": {
+                        "position": "resume"
+                      },
+                      "IDs - FILTRES": {
+                        "position": "tags"
+                      },
+                    "IMAGE": {
+                        "position": "image"
+                      }
+                    },
+                    "detail": {
+                      "NOM DE LA RESSOURCE": {
+                        "position": "title"
+                      },
+                      "DESCRIPTION": {
+                        "position": "resume"
+                      },
+                      "IMAGE": {
+                        "position": "image"
+                      },
+                      "LIEN": {
+                        "position": "links"
+                      },
+                      "IDs - FILTRES": {
+                        "position": "tags", "block_title": "Catégories :"
+                      }
+                    }
+                  }
+                }'
+                  onlypreview="false"
+                  usertoken: ""
+                  locale="fr"
+                ></datami-file> `;
+    
+    grid.innerHTML = html;
+    
+    document.getElementById('DatamiBloc').classList.toggle("hide");
+    document.getElementById('IndexBloc').classList.toggle("hide");
+    document.getElementsByClassName('md-sidebar')[0].classList.toggle("hide");
+}

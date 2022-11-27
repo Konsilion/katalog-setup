9a5cf6936857b0781fd4b3cbc3adbb19feb7c053
Nicolas BREMOND <99027754+Konsilion@users.noreply.github.com>
2022-11-28 00:09:42 +0100

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 const titre = document.querySelector("[titre-plateforme]")
 const descr = document.querySelector("[descr-plateforme]")
+const descr = document.querySelector("[logo-plateforme]")
 
 var url = window.location.pathname + './param.json';
 fetch(url)
@@ -14,7 +15,7 @@ function HomeConstruction(param) {
     
     titre.textContent = param.informations[0].name
     descr.textContent = param.informations[0].description
-    
+    titre.textContent = param.informations[0].logo
     
     document.getElementById('EditBtn').setAttribute("href", param.informations[0].link)
 }

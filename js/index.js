0c82a116b5b46591ed26bbc49ff426200cc8e120
Nicolas BREMOND <99027754+Konsilion@users.noreply.github.com>
2022-11-27 16:07:38 +0100

    Create index.js

---
+++ js/index.js
@@ -0,0 +1,19 @@
+const titre = document.querySelector("[titre-plateforme]")
+const descr = document.querySelector("[descr-plateforme]")
+
+var url = window.location.pathname + '../param.json';
+fetch(url)
+    .then(response => response.json())
+    .then(json => {
+        HomeConstruction(json);
+    });
+
+
+
+function HomeConstruction(param) {
+    
+    titre.textContent = param.informations[0].name
+    descr.textContent = param.informations[0].description
+    document.getElementById('EditBtn').setAttribute("href", param.informations[0].link + 'param.json')
+    
+}

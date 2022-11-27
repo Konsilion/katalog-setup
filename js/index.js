3e55ce929c19f4575cd745cd18032ad9629ff659
Nicolas BREMOND <99027754+Konsilion@users.noreply.github.com>
2022-11-27 22:48:08 +0100

    Update index.js

--- js/index.js
+++ js/index.js
@@ -14,6 +14,7 @@ function HomeConstruction(param) {
     
     titre.textContent = param.informations[0].name
     descr.textContent = param.informations[0].description
-    document.getElementById('EditBtn').setAttribute("href", param.informations[0].link + 'param.json')
-    document.getElementById('ParamBtn').setAttribute("href", param.informations[0].link + '../mkdocs.yml')
+    
+    
+    document.getElementById('EditBtn').setAttribute("href", param.informations[0].link)
 }

f73389486f6b6ddb0d099d3befdb6a78a94361a1
Nicolas BREMOND <99027754+Konsilion@users.noreply.github.com>
2022-11-29 12:59:54 +0100

    Update index.js

--- js/index.js
+++ js/index.js
@@ -23,15 +23,29 @@ linkRemoteCss('https://konsilion.github.io/katalog-setup/css/konsilion-mkdocs-md
 linkRemoteCss('https://konsilion.github.io/katalog-setup/css/konsilion-datami.css');
 
 
-
-
 document.getElementsByClassName('md-content')[0].innerHTML += `
 <p style="color:#AAA; font-size: 15px; text-align: center">
-    <span> Une plateforme open source codé avec 🤍 par <u><a href="https://konsilion.fr" target="_blank">Konsilion</a></u>.
-    <br>
-    Un grand merci à <u><a href="https://multi.coop" target="_blank"> multi </a></u> et <u><a href="https://squidfunk.github.io/mkdocs-material/" target="_blank"> mkdocs-material</a></u> pour le support technique.</span>
-    <br>
-    <br>
     <u><a href="https://konsilion.fr/wp/recherche-et-developpement/katalog-installation" target="_blank"> Obtenir une plateforme similaire</a></u>
+    &ensp;
+    <u><a style="cursor: pointer;" onclick="VisualParam();"> Paramètrages</a></u>
+    <br>
+    <br>
+    Une plateforme open-source codé avec 🤍 par <u><a href="https://konsilion.fr" target="_blank">Konsilion</a></u>.
+    <br><br>
+    Un grand merci à <u><a href="https://multi.coop" target="_blank"> multi</a></u> et <u><a href="https://squidfunk.github.io/mkdocs-material/" target="_blank"> mkdocs-material</a></u> pour le support technique   
 </p>
 `;
+
+
+function VisualParam() {
+    var url = window.location.pathname + '../konsilion.json';
+    fetch(url)
+    .then(response => response.json())
+    .then(json => {
+        GoToVisualParam(json.repo);
+    });
+}
+
+function GoToVisualParam(repo) {
+    window.open(repo + "/edit/master/mkdocs.yml");
+}

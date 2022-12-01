83af2e6168eb7c715b1027d8cdca55acd9ac90ef
Nicolas BREMOND <99027754+Konsilion@users.noreply.github.com>
2022-12-01 10:24:34 +0100

    Update index.js

--- js/index.js
+++ js/index.js
@@ -15,7 +15,7 @@ function linkRemoteCss(url){
 
 linkRemoteCss('https://konsilion.github.io/katalog-setup/css/konsilion-mkdocs.css');
 
-linkRemoteCss('https://konsilion.github.io/katalog-setup/css/konsilion-mkdocs-md.css');
+linkRemoteCss('../konsilion-mkdocs-md.css');
 
 
 
@@ -24,7 +24,7 @@ document.getElementsByClassName('md-content')[0].innerHTML += `
     id="LogoIndex"
     src="https://konsilion.fr/wp/wp-content/uploads/2022/04/Logo_Konsilion_V2-1024x325.png"
     style= "position: fixed;
-            top: 450px;
+            top: 600px;
             left: 25px;
             padding-top: 25px;
             max-width: 200px;
@@ -43,7 +43,7 @@ document.getElementsByClassName('md-content')[0].innerHTML += `
 <p style="color:#AAA; font-size: 15px; text-align: center">
     <u><a href="https://github.com/konsilion/katalog-template/" target="_blank"> Obtenir une plateforme similaire</a></u>
     &ensp;
-    <u><a style="cursor: pointer;" href="./etc/parametres/home"> Paramètrages</a></u>
+    <u><a style="cursor: pointer;" href="./admin"> Paramètrages</a></u>
     &ensp;
     <u><a style="cursor: pointer;" href="https://github.com/Konsilion/katalog-template/issues/new/choose" target="_blank"> Reporter une erreur</a></u>
     <br>

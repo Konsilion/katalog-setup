49d38065c10b7414f27170e963f21621acf21d00
Nicolas BREMOND <99027754+Konsilion@users.noreply.github.com>
2022-12-01 20:20:11 +0100

    Update pages.js

--- js/pages.js
+++ js/pages.js
@@ -44,15 +44,7 @@ fetch(url)
     document.getElementsByClassName('md-content')[0].innerHTML += `
         <img 
             id="LogoIndex"
-            src="` + json.logo + `"
-            style= "position: fixed;
-                    top: 45px;
-                    left: 25px;
-                    max-width: 200px;
-                    max-height: 45px;
-                    filter: grayscale(100%);
-                    opacity: 0.5;"
-        >
+            src="` + json.logo + `">
         <button class="ksln-btn-bottom" 
         onclick="window.open('https://github.com/` + json.user + `/` + json.repo +  `/edit/master/docs/pages/' + pages);"> 
         Modifier cette page

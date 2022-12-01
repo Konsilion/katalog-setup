a9a790c342ffe201aff384dba0389f9f70ea9b2b
Nicolas BREMOND <99027754+Konsilion@users.noreply.github.com>
2022-12-01 20:20:34 +0100

    Update katalog.js

--- js/katalog/katalog.js
+++ js/katalog/katalog.js
@@ -233,15 +233,7 @@ fetch(ksln_json)
         document.getElementsByClassName('md-content')[0].innerHTML += `
             <img 
                 id="LogoIndex"
-                src="` + json.logo + `"
-                style= "position: fixed;
-                        top: 45px;
-                        left: 25px;
-                        max-width: 200px;
-                        max-height: 45px;
-                        filter: grayscale(100%);
-                        opacity: 0.5;"
-            >
+                src="` + json.logo + `">
             <button class="ksln-btn-bottom" 
             onclick="window.open('https://github.com/` + json.user + `/` + json.repo +  `/edit/master/docs/katalog/` + katalog_folder + `/katalog.json');"> 
             Paramètrer ce catalogue

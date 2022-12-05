0b5cb2d8870830ea7e1412230ab63e330b7744b7
Nicolas BREMOND <99027754+Konsilion@users.noreply.github.com>
2022-12-05 11:41:53 +0100

    Update admin.js

--- js/admin.js
+++ js/admin.js
@@ -1,36 +1,3 @@
-function linkRemoteCss(url){
-
-    // create new link tag
-    var link = document.createElement('link');
-
-    // set properties of link tag
-    link.href = url;
-    link.rel = 'stylesheet';
-    link.type = 'text/css';
-
-    // append link element to html
-    document.body.appendChild(link);
-
-}
-
-linkRemoteCss('https://konsilion.github.io/katalog-setup/css/konsilion-mkdocs.css');
-
-linkRemoteCss('https://konsilion.github.io/katalog-setup/css/konsilion.css');
-
-
-document.getElementsByClassName('md-content')[0].innerHTML += `
-<p style="color:#AAA; font-size: 15px; text-align: center">
-    <u><a href="https://github.com/konsilion/katalog-template/" target="_blank"> Obtenir une plateforme similaire</a></u>
-    &ensp;
-    <br>
-    <br>
-    Une plateforme open-source codé avec 🤍 par <u><a href="https://konsilion.fr" target="_blank">Konsilion</a></u>.
-    <br><br>
-    Un grand merci à <u><a href="https://multi.coop" target="_blank"> multi</a></u> et <u><a href="https://squidfunk.github.io/mkdocs-material/" target="_blank"> mkdocs-material</a></u> pour le support technique   
-</p>
-`;
-
-
 var url = '/konsilion.json';
 fetch(url)
 .then(response => response.json())
@@ -46,13 +13,7 @@ fetch(url)
         onclick="window.open('https://github.com/` + json.user + `/` + json.repo +  `/upload/master/docs/stg');"> 
         Déposer un fichier
     </button>
-    `;
-    
-    document.getElementsByClassName('md-content')[0].innerHTML += `
-        <img 
-            id="LogoIndex" 
-            src="` + json.logo + `">
-        `;      
+    `;     
 });
 
 

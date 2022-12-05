f412ca06c31f3d2e471faa04cd0fdb6621e5ff02
Nicolas BREMOND <99027754+Konsilion@users.noreply.github.com>
2022-12-05 11:36:05 +0100

    Update konsilion.js

--- js/konsilion.js
+++ js/konsilion.js
@@ -29,3 +29,23 @@ fetch(url)
             src="` + json.logo + `">
         `;    
 });
+
+
+function HideNav() {
+    document.getElementById("HideNav").classList.toggle("hide");
+    document.getElementById("ShowNav").classList.toggle("hide");
+    document.getElementById("LogoIndex").classList.toggle("hide");
+    document.getElementsByClassName("md-sidebar--primary")[0].classList.toggle("hide");
+    document.getElementsByClassName("md-main")[0].style.marginRight = "auto";
+    document.getElementsByClassName("md-main")[0].style.marginLeft = "auto";
+    
+};
+
+function ShowNav() {
+    document.getElementById("HideNav").classList.toggle("hide");
+    document.getElementById("ShowNav").classList.toggle("hide");
+    document.getElementById("LogoIndex").classList.toggle("hide");
+    document.getElementsByClassName("md-sidebar--primary")[0].classList.toggle("hide");
+    document.getElementsByClassName("md-main")[0].style.marginRight = "0px";
+    document.getElementsByClassName("md-main")[0].style.marginLeft = "0px";
+};

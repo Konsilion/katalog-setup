ec8d61355d4bc91d49619c4e08f8312497521f94
Nicolas BREMOND <99027754+Konsilion@users.noreply.github.com>
2022-12-05 14:04:14 +0100

    Update pages.js

--- js/pages.js
+++ js/pages.js
@@ -1,6 +1,6 @@
 // ============ KONSILION JSON INFORMATIONS =============
 
-var url = '/konsilion.json';
+var url = window.location.pathname + '../../konsilion.json';
 fetch(url)
 .then(response => response.json())
 .then(json => {
@@ -13,24 +13,5 @@ fetch(url)
         onclick="window.open('https://github.com/` + json.user + `/` + json.repo +  `/edit/master/docs/pages/' + page);"> 
         Modifier cette page
         </button>
-        `;    
-});
-
-// ============ LOGO =============
-
-var url = window.location.pathname + '../../konsilion.json';
-fetch(url)
-.then(response => response.json())
-.then(json => {
-
-    var array = window.location.pathname.split('/');
-    var page = array[array.length-2];   
-    
-    document.getElementsByClassName('md-content')[0].innerHTML += `
-        <img 
-            id="LogoIndex"
-            src="` + json.logo + `"
-            onclick="window.open(window.location.pathname + '../../','_self')"
-            style="cursor: pointer;">
         `;    
 });

3be382234837bf8d39e43e48cd7b2b788ec6937a
Nicolas BREMOND <99027754+Konsilion@users.noreply.github.com>
2022-12-05 11:55:51 +0100

    Update konsilion.js

--- js/konsilion.js
+++ js/konsilion.js
@@ -26,9 +26,9 @@ linkRemoteCss('https://konsilion.github.io/katalog-setup/css/konsilion.css');
 // ============= PIED DE PAGE ================
 
 document.getElementsByClassName('md-content')[0].innerHTML += `
-<button id="ShowNav" onclick="ShowNav();" class="hide">Afficher le menu<button>
+<button id="ShowNav" onclick="ShowNav();" class="hide">Afficher le menu</button>
 
-<button id="HideNav" onclick="HideNav();">Masquer<button>
+<button id="HideNav" onclick="HideNav();">Masquer</button>
 
 <p style="color:#AAA; font-size: 15px; text-align: center">
     <u><a href="https://github.com/konsilion/katalog-template/" target="_blank"> Obtenir une plateforme similaire</a></u>

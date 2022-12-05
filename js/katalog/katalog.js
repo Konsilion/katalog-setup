0936c431a2eeb9657baa7c35f36aa1f3610499e0
Nicolas BREMOND <99027754+Konsilion@users.noreply.github.com>
2022-12-05 13:56:58 +0100

    Update katalog.js

--- js/katalog/katalog.js
+++ js/katalog/katalog.js
@@ -243,4 +243,26 @@ fetch(ksln_json)
     });
 
 
+// ============ LOGO =============
+
+var url = window.location.pathname + '../../../konsilion.json';
+fetch(url)
+.then(response => response.json())
+.then(json => {
+
+    var array = window.location.pathname.split('/');
+    var page = array[array.length-2];   
+    
+    document.getElementsByClassName('md-content')[0].innerHTML += `
+        <img 
+            id="LogoIndex"
+            src="` + json.logo + `"
+            onclick="window.open(window.location.pathname + '../../../','_self')"
+            style="cursor: pointer;">
+        `;    
+});
+
+
+
+
 TakeTheJson();

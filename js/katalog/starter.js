a28fe83736deb935695dd001dfd1d3f45cc6df7c
Nicolas BREMOND <99027754+Konsilion@users.noreply.github.com>
2022-12-05 11:42:16 +0100

    Update starter.js

--- js/katalog/starter.js
+++ js/katalog/starter.js
@@ -1,28 +1,3 @@
-function linkRemoteCss(url){
-
-// create new link tag
-var link = document.createElement('link');
-
-// set properties of link tag
-link.href = url;
-link.rel = 'stylesheet';
-link.type = 'text/css';
-
-// append link element to html
-document.body.appendChild(link);
-
-}
-
-linkRemoteCss('https://konsilion.github.io/katalog-setup/css/konsilion-mkdocs.css');
-
-linkRemoteCss('https://konsilion.github.io/katalog-setup/css/konsilion-datami.css');
-
-linkRemoteCss('https://konsilion.github.io/katalog-setup/css/konsilion.css');
-
-
-
-
-
 fetch("https://unpkg.com/papaparse@5.3.0/papaparse.min.js")
   .then((response) => response.text())
   .then((text) => eval(text))
@@ -51,30 +26,3 @@ fetch("https://datami-widget.multi.coop/js/app.js")
   .then((text) => eval(text))
   .then(() => {
   })
-
-
-
-
-
-
-
-
-
-function HideNav() {
-    document.getElementById("HideNav").classList.toggle("hide");
-    document.getElementById("ShowNav").classList.toggle("hide");
-    document.getElementById("LogoIndex").classList.toggle("hide");
-    document.getElementsByClassName("md-sidebar--primary")[0].classList.toggle("hide");
-    document.getElementsByClassName("md-main")[0].style.marginRight = "auto";
-    document.getElementsByClassName("md-main")[0].style.marginLeft = "auto";
-    
-};
-
-function ShowNav() {
-    document.getElementById("HideNav").classList.toggle("hide");
-    document.getElementById("ShowNav").classList.toggle("hide");
-    document.getElementById("LogoIndex").classList.toggle("hide");
-    document.getElementsByClassName("md-sidebar--primary")[0].classList.toggle("hide");
-    document.getElementsByClassName("md-main")[0].style.marginRight = "0px";
-    document.getElementsByClassName("md-main")[0].style.marginLeft = "0px";
-};

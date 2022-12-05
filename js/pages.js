1e37d150ace734fa453bb6a9f749839fd6e290b5
Nicolas BREMOND <99027754+Konsilion@users.noreply.github.com>
2022-12-05 11:40:48 +0100

    Update pages.js

--- js/pages.js
+++ js/pages.js
@@ -1,27 +1,3 @@
-// ============= IMPORT CSS =============
-
-function linkRemoteCss(url){
-
-    // create new link tag
-    var link = document.createElement('link');
-
-    // set properties of link tag
-    link.href = url;
-    link.rel = 'stylesheet';
-    link.type = 'text/css';
-
-    // append link element to html
-    document.body.appendChild(link);
-
-}
-
-linkRemoteCss('https://konsilion.github.io/katalog-setup/css/konsilion-mkdocs.css');
-
-linkRemoteCss('https://konsilion.github.io/katalog-setup/css/konsilion.css');
-
-
-
-
 // ============ KONSILION JSON INFORMATIONS =============
 
 var url = '/konsilion.json';

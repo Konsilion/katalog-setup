785842222e98632976928c26f52cc754ff685172
Nicolas BREMOND <99027754+Konsilion@users.noreply.github.com>
2022-12-05 11:27:28 +0100

    Update starter.js

--- js/katalog/starter.js
+++ js/katalog/starter.js
@@ -1,11 +1,34 @@
+function linkRemoteCss(url){
+
+// create new link tag
+var link = document.createElement('link');
+
+// set properties of link tag
+link.href = url;
+link.rel = 'stylesheet';
+link.type = 'text/css';
+
+// append link element to html
+document.body.appendChild(link);
+
+}
+
+linkRemoteCss('https://konsilion.github.io/katalog-setup/css/konsilion-mkdocs.css');
+
+linkRemoteCss('https://konsilion.github.io/katalog-setup/css/konsilion-datami.css');
+
+linkRemoteCss('https://konsilion.github.io/katalog-setup/css/konsilion.css');
+
+
+
+
+
 fetch("https://unpkg.com/papaparse@5.3.0/papaparse.min.js")
   .then((response) => response.text())
   .then((text) => eval(text))
   .then(() => {
   
   })
-
-
 
 
 fetch("https://konsilion.github.io/katalog-setup/js/katalog/katalog.js")
@@ -34,23 +57,24 @@ fetch("https://datami-widget.multi.coop/js/app.js")
 
 
 
-function linkRemoteCss(url){
 
-// create new link tag
-var link = document.createElement('link');
 
-// set properties of link tag
-link.href = url;
-link.rel = 'stylesheet';
-link.type = 'text/css';
 
-// append link element to html
-document.body.appendChild(link);
+function HideNav() {
+    document.getElementById("HideNav").classList.toggle("hide");
+    document.getElementById("ShowNav").classList.toggle("hide");
+    document.getElementById("LogoIndex").classList.toggle("hide");
+    document.getElementsByClassName("md-sidebar--primary")[0].classList.toggle("hide");
+    document.getElementsByClassName("md-main")[0].style.marginRight = "auto";
+    document.getElementsByClassName("md-main")[0].style.marginLeft = "auto";
+    
+};
 
-}
-
-linkRemoteCss('https://konsilion.github.io/katalog-setup/css/konsilion-mkdocs.css');
-
-linkRemoteCss('https://konsilion.github.io/katalog-setup/css/konsilion-datami.css');
-
-linkRemoteCss('https://konsilion.github.io/katalog-setup/css/konsilion.css');
+function ShowNav() {
+    document.getElementById("HideNav").classList.toggle("hide");
+    document.getElementById("ShowNav").classList.toggle("hide");
+    document.getElementById("LogoIndex").classList.toggle("hide");
+    document.getElementsByClassName("md-sidebar--primary")[0].classList.toggle("hide");
+    document.getElementsByClassName("md-main")[0].style.marginRight = "0px";
+    document.getElementsByClassName("md-main")[0].style.marginLeft = "0px";
+};

1eb338179aa418603172ce9aa251944c9e4dd46b
Nicolas BREMOND <99027754+Konsilion@users.noreply.github.com>
2022-12-08 20:41:37 +0100

    Create footer.js

---
+++ js/footer.js
@@ -0,0 +1,15 @@
+// ============= PIED DE PAGE ================
+
+document.getElementsByClassName('md-content')[0].innerHTML += `
+<button id="ShowNav" onclick="ShowNav();" class="hide">Afficher le menu</button>
+<button id="HideNav" onclick="HideNav();"> x </button>
+<p style="color:#AAA; font-size: 15px; text-align: center">
+    <u><a href="https://github.com/konsilion/katalog-template/" target="_blank"> Obtenir une plateforme similaire</a></u>
+    &ensp;
+    <br>
+    <br>
+    Une plateforme open-source codé avec 🤍 par Konsilion.
+    <br><br>
+    Un grand merci à <u><a href="https://multi.coop" target="_blank"> multi</a></u> et <u><a href="https://squidfunk.github.io/mkdocs-material/" target="_blank"> mkdocs-material</a></u> pour le support technique   
+</p>
+`;

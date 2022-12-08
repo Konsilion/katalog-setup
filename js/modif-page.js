29bea90e43b474ac6d29537830a4cd1444a76845
Nicolas BREMOND <99027754+Konsilion@users.noreply.github.com>
2022-12-08 21:17:48 +0100

    Update modif-page.js

--- js/modif-page.js
+++ js/modif-page.js
@@ -1,16 +1,26 @@
 // ============ KONSILION JSON INFORMATIONS =============
 
-var url = window.location.protocol + `//` + window.location.host + `/` + window.location.pathname.split('/')[1];
+var url = window.location.protocol + `//` + window.location.host + `/` + window.location.pathname.split('/')[0];
+
 fetch(url + '/konsilion.json')
 .then(response => response.json())
 .then(json => {
 
     var array = window.location.pathname.split('/');
-    var page = array[array.length-2];   
-    
+
+    let page = ""
+
+    for (let i = 0; i < (array.length - 2); i++) {
+        i = i+1;
+
+        page += '/' + array[i]
+        
+        i = i-1;
+    }
+          
     document.getElementsByClassName('md-content')[0].innerHTML += `
         <button class="ksln-btn-bottom" 
-        onclick="window.open('` + url + `/pages/` + page + `.md');"> 
+        onclick="window.open('https://github.com/` + json.user + `/` + json.repo +  `/edit/master/docs` + page + `.md');">  
         Modifier cette page
         </button>
         `;    

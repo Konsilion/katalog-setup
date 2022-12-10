f139536b7b38e3790f21ebc170f133115faaae36
Nicolas BREMOND <99027754+Konsilion@users.noreply.github.com>
2022-12-10 23:15:30 +0100

    change button of modif katalog

--- js/katalog/katalog.js
+++ js/katalog/katalog.js
@@ -197,27 +197,63 @@ function DatamiKatalog(num,type_datami,title,descr,gitfile,model,cardview,token)
 
 }
 
-fetch(ksln_json)
-    .then(response => response.json())
-    .then(json => {    
-        document.getElementsByClassName('md-content')[0].innerHTML += `
-            <button class="ksln-btn-bottom" 
-            onclick="window.open('https://github.com/` + json.user + `/` + json.repo +  `/edit/master/docs/katalog/` + katalog_folder + `/katalog.json');"> 
-            Paramètrer ce catalogue
-            </button>
-            `;
-    
-            main_gitfile = `https://github.com/` + json.user + `/` + json.repo + `/` + `blob/master/docs/katalog/` + katalog_folder + `/data.csv`
-    
-            main_token = json.token;
-    });
-
-
 
 
 TakeTheJson();
 
 
 
+
+
+
+// ============ KONSILION JSON INFORMATIONS =============
+
+var url = window.location.protocol + `//` + window.location.host + `/` + window.location.pathname.split('/')[1];
+
+fetch(url + '/konsilion.json')
+.then(response => response.json())
+.then(json => {
+
+    var array = window.location.pathname.split('/');
+
+    let page = ""
+
+    for (let i = 2; i < (array.length - 3); i++) {
+        i = i+1;
+
+        page += '/' + array[i]
+        
+        i = i-1;
+    } 
+          alert("");
+        document.getElementsByClassName('md-content')[0].innerHTML += `
+        <button class="ksln-btn-bottom" 
+        onclick="HideShow('` + json.user + `','` + json.repo + `','` + page + `');">  
+        Modifier cette page
+        </button>
+        `;    
+});
+
+function HideShow(user,repo,page) {
+
+    let html = `<button class="ksln-btn-bottom" 
+                onclick="window.location.reload();">  
+                Annuler le paramètrage
+                </button>
+    
+                <datami-file
+                  title="Schéma des données de l-observatoire"
+                  gitfile="https://github.com/` + user + `/` + repo +  `/edit/master/docs` + page + `/katalog.json"
+                  options='{
+                  "defaultDepth": 2,
+                  "allowKeyEdit": true
+                }'
+                  onlypreview="false"
+                  locale="fr"
+                ></datami-file>
+                `;
+    
+    document.getElementsByClassName('md-content')[0].innerHTML = html;  
+};
 
 

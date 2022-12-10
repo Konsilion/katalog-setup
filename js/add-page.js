d4c3b35d7e7a70654dbe9cbe0624e51ca91dc5b1
Nicolas BREMOND <99027754+Konsilion@users.noreply.github.com>
2022-12-11 00:30:49 +0100

    Create add-page.js

---
+++ js/add-page.js
@@ -0,0 +1,27 @@
+// ============ KONSILION JSON INFORMATIONS =============
+
+var url = window.location.protocol + `//` + window.location.host + `/` + window.location.pathname.split('/')[1];
+
+fetch(url + '/konsilion.json')
+.then(response => response.json())
+.then(json => {
+
+    var array = window.location.pathname.split('/');
+
+    let page = ""
+
+    for (let i = 1; i < (array.length - 3); i++) {
+        i = i+1;
+
+        page += '/' + array[i]
+        
+        i = i-1;
+    } 
+
+        document.getElementsByClassName('md-content')[0].innerHTML += `
+        <button class="ksln-btn-top"  
+        onclick="window.open('https://github.com/` + json.user + `/` + json.repo +  `/new/master/docs/pages');">  
+        Ajouter une page
+        </button>
+        `;    
+});

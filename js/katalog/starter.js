7a2a0560f7e35bb9583590ffdf45b9044cd12ebb
Nicolas BREMOND <99027754+Konsilion@users.noreply.github.com>
2022-12-10 23:45:52 +0100

    Update starter.js

--- js/katalog/starter.js
+++ js/katalog/starter.js
@@ -18,3 +18,27 @@ fetch("https://datami-widget.multi.coop/js/app.js")
   .then((text) => eval(text))
   .then(() => {
   })
+
+
+
+function HideShow(user,repo,page) {
+
+    let html = `<button class="ksln-btn-bottom" 
+                onclick="window.location.reload();">  
+                Annuler le paramètrage
+                </button>
+    
+                <datami-file
+                  title="Schéma des données de l-observatoire"
+                  gitfile="https://github.com/` + user + `/` + repo +  `/edit/master/docs` + page + `/katalog.json"
+                  options='{
+                  "defaultDepth": 2,
+                  "allowKeyEdit": true
+                }'
+                  onlypreview="false"
+                  locale="fr"
+                ></datami-file>
+                `;
+    
+    document.getElementsByClassName('md-content')[0].innerHTML = html;  
+};

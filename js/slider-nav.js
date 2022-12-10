7191954c59eb865e119eaac557be82604a18b2b1
Nicolas BREMOND <99027754+Konsilion@users.noreply.github.com>
2022-12-11 00:14:40 +0100

    Update slider-nav.js

--- js/slider-nav.js
+++ js/slider-nav.js
@@ -11,6 +11,7 @@ function HideNav() {
     document.getElementById("ShowNav").classList.toggle("hide");
     document.getElementsByClassName("md-header__inner md-grid")[0].classList.toggle("hide");
     document.getElementsByClassName("md-sidebar--primary")[0].classList.toggle("hide");
+    document.getElementsByClassName("ksln-btn-bottom")[0].classList.toggle("hide");
     document.getElementsByClassName("md-main")[0].style.marginRight = "auto";
     document.getElementsByClassName("md-main")[0].style.marginLeft = "auto";
     
@@ -21,6 +22,7 @@ function ShowNav() {
     document.getElementById("ShowNav").classList.toggle("hide");
     document.getElementsByClassName("md-header__inner md-grid")[0].classList.toggle("hide");
     document.getElementsByClassName("md-sidebar--primary")[0].classList.toggle("hide");
+    document.getElementsByClassName("ksln-btn-bottom")[0].classList.toggle("hide");
     document.getElementsByClassName("md-main")[0].style.marginRight = "0px";
     document.getElementsByClassName("md-main")[0].style.marginLeft = "0px";
 };

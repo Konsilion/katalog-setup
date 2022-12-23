55679e269828046786b0e6ed7ab357faa4e4abbd
Nicolas BREMOND <99027754+Konsilion@users.noreply.github.com>
2022-12-23 20:08:31 +0100

    suppression PapaParse

--- js/katalog/starter.js
+++ js/katalog/starter.js
@@ -1,11 +1,3 @@
-fetch("https://unpkg.com/papaparse@5.3.0/papaparse.min.js")
-  .then((response) => response.text())
-  .then((text) => eval(text))
-  .then(() => {
-  
-  })
-
-
 fetch("https://konsilion.github.io/katalog-setup/js/katalog/katalog.js")
   .then((response) => response.text())
   .then((text) => eval(text))

80c0cfabe2be5bab83def52449e1f468a325f63f
Nicolas BREMOND <99027754+Konsilion@users.noreply.github.com>
2022-12-29 18:08:16 +0100

    Update modif-page.js

--- js/functionality/modif-page.js
+++ js/functionality/modif-page.js
@@ -35,7 +35,7 @@ function ModifPage(user,repo,page,token) {
 
     var style = document.createElement('style');
         style.type = 'text/css';
-        style.innerHTML = '.md-content {margin: 50px auto; max-width: 100% !important; padding: 0 25px;}';
+        style.innerHTML = '.md-content {margin: 50px auto; max-width: 100% !important; padding: 0 25px;} .md-content {margin: 50px auto; max-width: 100vw !important; padding: 0 25px;}';
         document.getElementsByTagName('head')[0].appendChild(style);
 
     // ---------------------

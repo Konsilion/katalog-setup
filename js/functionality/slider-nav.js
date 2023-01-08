8711be91817cb196213b172fb638d889b246993c
Nicolas BREMOND <99027754+Konsilion@users.noreply.github.com>
2023-01-08 23:49:30 +0100

    delete ShowNav

--- js/functionality/slider-nav.js
+++ js/functionality/slider-nav.js
@@ -1,7 +1,7 @@
 // =======  SHOW/HIDE NAVIGATION LATERALE ====
 
 document.getElementsByClassName('md-header')[0].innerHTML += `
-<button id="ShowNav" onclick="ShowNav();" class="hide">Afficher le menu</button>
+<button id="ShowNav" onclick="window.location.reload();" class="hide">Afficher le menu</button>
 <button id="HideNav" onclick="HideNav();"> x </button>
 `;
 
@@ -33,13 +33,5 @@ function HideNav() {
 };
 
 function ShowNav() {
-    document.getElementById("HideNav").classList.toggle("hide");
-    document.getElementById("ShowNav").classList.toggle("hide");
-    document.getElementsByClassName("md-header__inner md-grid")[0].classList.toggle("hide");
-    document.getElementsByClassName("md-sidebar--primary")[0].classList.toggle("hide");
-    document.getElementsByClassName("md-tabs")[0].classList.toggle("hide");
-    document.getElementsByClassName("md-main")[0].style.marginRight = "0px";
-    document.getElementsByClassName("md-main")[0].style.marginLeft = "0px";
-    document.getElementsByClassName("ksln-btn-top")[0].classList.toggle("hide");
-    document.getElementsByClassName("ksln-btn-bottom")[0].classList.toggle("hide");
+    
 };

2bec5347f5df0f6b6d18e492e60745376bd8d1b9
Nicolas BREMOND <99027754+Konsilion@users.noreply.github.com>
2023-01-08 23:07:19 +0100

    Update slider-nav.js

--- js/functionality/slider-nav.js
+++ js/functionality/slider-nav.js
@@ -7,16 +7,29 @@ document.getElementsByClassName('md-header')[0].innerHTML += `
 
 
 function HideNav() {
-    document.getElementById("HideNav").classList.toggle("hide");
-    document.getElementById("ShowNav").classList.toggle("hide");
-    document.getElementsByClassName("md-header__inner md-grid")[0].classList.toggle("hide");
-    document.getElementsByClassName("md-sidebar--primary")[0].classList.toggle("hide");
-    document.getElementsByClassName("md-tabs")[0].classList.toggle("hide");
+    // document.getElementById("HideNav").classList.toggle("hide");
+    // document.getElementById("ShowNav").classList.toggle("hide");
+    // document.getElementsByClassName("md-header__inner md-grid")[0].classList.toggle("hide");
+    // document.getElementsByClassName("md-sidebar--primary")[0].classList.toggle("hide");
+    // document.getElementsByClassName("md-tabs")[0].classList.toggle("hide");
     document.getElementsByClassName("md-main")[0].style.marginRight = "auto";
     document.getElementsByClassName("md-main")[0].style.marginLeft = "auto";    
-    document.getElementsByClassName("ksln-btn-top")[0].classList.toggle("hide");
-    document.getElementsByClassName("ksln-btn-bottom")[0].classList.toggle("hide");
-    
+    //document.getElementsByClassName("ksln-btn-top")[0].classList.toggle("hide");
+    //document.getElementsByClassName("ksln-btn-bottom")[0].classList.toggle("hide");
+
+    // Ajout CSS ---------------------
+
+    var style = document.createElement('style');
+        style.type = 'text/css';
+        style.innerHTML = ` .md-header__inner md-grid {display: none !important;} 
+                            .md-sidebar--primary {display: none !important;} 
+                            .md-tabs {display: none !important;}    
+                            .ksln-btn-top {display: none !important;} 
+                            .ksln-btn-bottom {display: none !important;} 
+                            #HideNav {display: none}
+                            #ShowNav {display: block}
+        document.getElementsByTagName('head')[0].appendChild(style);    
+    // ---------------------
 };
 
 function ShowNav() {

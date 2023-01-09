f77924eb31df58c375cbc83624de8b30094428ba
Nicolas BREMOND <99027754+Konsilion@users.noreply.github.com>
2023-01-09 01:46:14 +0100

    Update katalog.js

--- js/katalog/katalog.js
+++ js/katalog/katalog.js
@@ -244,7 +244,7 @@ function DatamiKatalog(num,type_datami,title,descr,gitfile,model,cardview,token,
     html_end = `onlypreview="false"
                 usertoken="` + window.atob(token) + `"
                 locale="fr"
-        ></datami-file><br><h   r> `;
+        ></datami-file><br><hr> `;
  
     var html_final = ""
 

31701b27c26d702a15f5eeb5425d723931fd2d98
Nicolas BREMOND <99027754+Konsilion@users.noreply.github.com>
2023-01-21 13:50:48 +0100

    Update starter.js

--- js/katalog/starter.js
+++ js/katalog/starter.js
@@ -291,39 +291,3 @@ fetch("https://konsilion.github.io/katalog-setup/js/katalog/katalog.js")
   })  
 
 
-
-function ModifKatalog(user,repo,page,token) {
-
-    let html = `<button class="ksln-btn-top" style="background-color: #bd0000; color: white;" 
-                onclick="window.location.reload();">  
-                Quitter
-                </button>
-    
-                <datami-file
-                  title="Parametrage du catalogue Web"
-                  gitfile="https://github.com/` + user + `/` + repo +  `/edit/master/docs` + page + `/katalog.json"
-                  options='{
-                  "defaultDepth": 2,
-                  "allowKeyEdit": true
-                }'
-                  onlypreview="false"
-                  usertoken="` + window.atob(token) + `"
-                  locale="fr"
-                ></datami-file>
-                `;
-    
-    document.getElementsByClassName('md-content')[0].innerHTML = html;
-  
-    // Ajout CSS ---------------------
-
-    var style = document.createElement('style');
-        style.type = 'text/css';
-        style.innerHTML = ` .md-sidebar {display: none !important;} 
-                            .md-content {margin: 50px auto; max-width: 100vw !important; padding: 0 25px;}
-                            #ModifKatalog {display: none}
-                            #HideNav {display: none}
-                            #AddPage {display: none}`;
-        document.getElementsByTagName('head')[0].appendChild(style);    
-    // --------------------- 
-  
-};

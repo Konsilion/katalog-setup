339ca273da37f0b9b73c961dc0569ff0c9775d95
Nicolas BREMOND <99027754+Konsilion@users.noreply.github.com>
2023-01-23 12:14:37 +0100

    Update admin.js

--- js/admin.js
+++ js/admin.js
@@ -1,43 +1,5 @@
-var url = window.location.pathname + '../konsilion.json';
-fetch(url)
-.then(response => response.json())
-.then(json => {
-    
-    var url_repo = "https://github.com/" + json.user + "/" + json.repo
-    
-    document.getElementById("IndexLink").setAttribute("href", url_repo + "/edit/master/docs/pages/accueil.md");
-    document.getElementById("PageLink").setAttribute("href", url_repo + "/new/master/docs/pages");
-    document.getElementById("InitLink").setAttribute("href", url_repo + "/edit/master/docs/konsilion.json");
-    document.getElementById("ConfigLink").setAttribute("href", url_repo +  "/edit/master/mkdocs.yml"); 
-});
+var MainVersion = "1.0"
 
-
-function myFunction() {
-    // Copy the text inside the text field
-    navigator.clipboard.writeText(`# Titre de votre page
-    
-
-
-
-
-
-
-
-
-
-
-
-    
-    
-    
-    
-    
-    ---
-
-    <script type="text/javascript" src="https://konsilion.github.io/katalog-setup/js/pages.js"></script>
-    <script type="text/javascript" src="https://konsilion.github.io/katalog-setup/js/konsilion.js"></script>
-    `);
-
-    // Alert the copied text
-    alert("Scripts copié et à coller dans l'étape suivante au bas de la page.");
+if ( MainVersion != LocalVersion){
+    document.getElementById("UpdateAlert").style.display = "block";
 }

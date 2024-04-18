e315e594ccc752674eb9f935ebd74add0f30e991
Nicolas BREMOND <99027754+Konsilion@users.noreply.github.com>
2024-04-18 21:12:58 +0200

    Update page-add.js

--- js/functionality/page-add.js
+++ js/functionality/page-add.js
@@ -1,6 +1,6 @@
 document.getElementsByClassName('md-header')[0].innerHTML += `
 <button class="ksln-btn-bottom" id="AddPage" 
-onclick="window.open('` + URLKatalogRepo +  `/new/master/docs/pages');">  
+onclick="window.open('` + URLKatalogRepo +  `/new/master/mkdocs/docs/pages');">  
 Créer une page
 </button>
 `;    
